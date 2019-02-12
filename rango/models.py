c86b8d62a96270f8a89eb2268104881811dc32de
HanLuo <1903851304@qq.com>
2019-02-12 18:02:15 +0800

    update

--- rango/models.py
+++ rango/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
 
-    name = models.CharField(max_length = 128, unique= True)
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -15,7 +19,7 @@ class Page(models.Model):
     # ForeignKey ：用于建立一对多关系。
     # OneToOneField ：用于建立一对一关系。
     # ManyToManyField ：用于建立多对多关系。
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
