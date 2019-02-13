60304680987df07bb5acd6b265f73251df220de0
HanLuo <1903851304@qq.com>
2019-02-13 14:24:13 +0800

    update

--- populate_rango.py
+++ populate_rango.py
@@ -1,61 +1,67 @@
 import sys
-# sys.path.append('..')
-
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango.settings')
-
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango.settings')
 django.setup()
+
+
 from rango.models import Category, Page
 
-def add_cat(name):
-     c = Category.objects.get_or_create(name=name)[0]
-     c.save()
-     return c
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
 
 def add_page(cat, title, url, views=0):
-     p = Page.objects.get_or_create(category=cat, title=title)[0]
-     p.url = url
-     p.views = views
-     p.save()
-     return p
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url = url
+    p.views = views
+    p.save()
+    return p
+
 
 def populate():
-     python_pages = [ \
-     {"title": "Official Python Tutorial", \
-     "url":"http://docs.python.org/2/tutorial/"}, \
-     {"title":"How to Think like a Computer Scientist", \
-     "url":"http://www.greenteapress.com/thinkpython/"}, \
-     {"title":"Learn Python in 10 Minutes", \
-     "url":"http://www.korokithakis.net/tutorials/python/"}]
+    python_pages = [
+        {"title": "Official Python Tutorial",
+         "url": "http://docs.python.org/2/tutorial/"},
+        {"title": "How to Think like a Computer Scientist",
+         "url": "http://www.greenteapress.com/thinkpython/"},
+        {"title": "Learn Python in 10 Minutes",
+         "url": "http://www.korokithakis.net/tutorials/python/"}]
 
-     django_pages = [ \
-     {"title":"Official Django Tutorial", \
-     "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"}, \
-     {"title":"Django Rocks", \
-     "url":"http://www.djangorocks.com/"}, \
-     {"title":"How to Tango with Django", \
-     "url":"http://www.tangowithdjango.com/"} ]
+    django_pages = [
+        {"title": "Official Django Tutorial",
+         "url": "https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        {"title": "Django Rocks",
+         "url": "http://www.djangorocks.com/"},
+        {"title": "How to Tango with Django",
+         "url": "http://www.tangowithdjango.com/"}]
 
-     other_pages = [ \
-     {"title":"Bottle", \
-     "url":"http://bottlepy.org/docs/dev/"}, \
-     {"title":"Flask", \
-     "url":"http://flask.pocoo.org"} ]
+    other_pages = [
+        {"title": "Bottle",
+         "url": "http://bottlepy.org/docs/dev/"},
+        {"title": "Flask",
+         "url": "http://flask.pocoo.org"}]
 
-     cats = {"Python": {"pages": python_pages}, \
-     "Django": {"pages": django_pages}, \
-     "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages},
+            "Django": {"pages": django_pages},
+            "Other Frameworks": {"pages": other_pages}}
 
-     for cat, cat_data in cats.items():
-          c = add_cat(cat)
-          for p in cat_data["pages"]:
-               add_page(c, p['title'], p['url'])
-     
-     for c in Category.objects.all():
-          for p in Page.objects.filter(category = c):
-               print ("-{0} - {1}".format(str(c), str(p)))
+    cats_views_likes = {"Python": {'views': 128, 'likes': 64}, "Django": {
+        'views': 64, 'likes': 23}, "Other Frameworks": {'views': 32, 'likes': 16}}
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, cats_views_likes[cat]
+                    ['views'], cats_views_likes[cat]['likes'])
+        for p in cat_data["pages"]:
+            add_page(c, p['title'], p['url'])
+
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print("-{0} - {1}".format(str(c), str(p)))
 
 
 if __name__ == "__main__":
--- rango/models.py
+++ rango/models.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 class Category(models.Model):
 
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -24,7 +26,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    date = models.DateTimeField(default = timezone.now)
+    date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
