4342790f30519ccaf04246989f5f2826adf1d90b
HanLuo <1903851304@qq.com>
2019-02-13 15:03:13 +0800

    update

--- rango/admin.py
+++ rango/admin.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 from rango.models import Category
 from rango.models import Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'date')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    # return HttpResponse('Rango!')
+    # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # # return HttpResponse('Rango!')
+    # return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
