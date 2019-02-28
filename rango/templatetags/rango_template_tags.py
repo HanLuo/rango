fe137cfba039b08c821ae06eba1c69c4cae07b86
HanLuo <1903851304@qq.com>
2019-02-28 14:10:40 +0800

    fix bug

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -1,9 +1,9 @@
 from django import template
-from tango.models import Category
+from rango.models import Category
 
 
 register = template.Library()
 
-@register.inclusion_tag('tango.cats.html')
+@register.inclusion_tag('rango/cats.html')
 def get_category_list():
     return {'cats':Category.objects.all()}
