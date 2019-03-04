e0dbb3d127af09e4d9477019cef47ac1a1165fb0
HanLuo <1903851304@qq.com>
2019-03-04 16:22:28 +0800

    添加USER模型

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -38,3 +39,13 @@ class Page(models.Model):
         return self.title
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    # 增加的属性
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # 覆盖 __str__方法
+    def __str__(self):
+        return self.user.username
