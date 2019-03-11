36fd8eb2f2f0ecde9c0f4f13a74692764c8ac420
1903851304@qq.com <1903851304@qq.com>
2019-03-11 22:11:18 +0800

    sessions

--- rango/views.py
+++ rango/views.py
@@ -24,23 +24,44 @@ def index(request):
     request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list , 'visits': int(request.COOKIES.get('visits', '1'))}
+    visitor_cookie_handler(request)
+    context_dict = {'categories': category_list, 'pages': page_list , 'visits': int(request.session['visits'])}
     response =  render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request, response)
     return response
 
-def visitor_cookie_handler(request, response):
-    print ('visitor_cookie_handler')
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+# session
+def get_server_side_cookie(request, cookie, default_val = None):
+    val = request.session.get(cookie)
+    return val if val else default_val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
         visits += 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
-    response.set_cookie('visits', visits)
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
+
+
+
+# cookies
+# def visitor_cookie_handler(request, response):
+#     print ('visitor_cookie_handler')
+#     visits = int(request.COOKIES.get('visits', '1'))
+#     last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+#     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+#     if (datetime.now() - last_visit_time).days > 0:
+#         visits += 1
+#         response.set_cookie('last_visit', str(datetime.now()))
+#     else:
+#         response.set_cookie('last_visit', last_visit_cookie)
+#     response.set_cookie('visits', visits)
 
 def about(request):
     # 测试cookies是否工作
