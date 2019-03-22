1a557bfe10e32e084727999f9e4a65edd01b3650
HanLuo <1903851304@qq.com>
2019-03-22 16:34:12 +0800

    update

--- rango/google_search.py
+++ rango/google_search.py
@@ -1,39 +1,60 @@
 import json
 import urllib.parse
 import urllib.request
+import os
+import socket
 
 
 def read_google_key():
     google_search_key = None
+    google_api_key = None
 
+    # 获得当前目录
+    # path = os.getcwd()
+    path = os.path.abspath(os.path.dirname(__file__))
+    print(path)
+
+    url = ('{path}//..//key//{name}').format(path=path, name='search.key')
     try:
-        with open('search.txt', 'r') as f:
-            google_search_key = f.readlines().strip()
+        with open(url, 'r') as f:
+            google_search_key = f.readline().strip()
     except:
         raise IOError('search.key file not found!')
 
-    return google_search_key
+    url = ('{path}//..//key//{name}').format(path=path, name='api.key')
+    try:
+        with open(url, 'r') as f:
+            google_api_key = f.readline().strip()
+    except:
+        raise IOError('api.key file not found!')
+
+    return google_search_key, google_api_key
+
 
 def run_query(search_terms, size=10):
     google_key = read_google_key()
-
     if not google_key:
         raise IOError('google_key not found!')
-    root_url = 'https:// cse.google.com/cse'
+    root_url = 'https://www.googleapis.com/customsearch/v1'
 
     # 处理查询的字符串
     query_string = urllib.parse.quote(search_terms)
 
-    search_url = ('{root_url}?cx={cx}&q={query}').format(root_url = root_url, cx=google_key, query=query_string)
-    print (search_url)
+    search_url = ('{root_url}?key={key}&cx={cx}&q={q}').format(
+        root_url=root_url, key=google_key[1], cx=google_key[0], q=query_string)
+    print(search_url)
 
-    results = []
-    try:
-        response = urllib.request.urlopen(search_url).read().decode('utf-8')
-        json_response = json.loads(response)
-        print (json_response)
-    except:
-        pass
+    # 因不可抗力因素，要使用代理
+    # timeout = 2
+    # socket.setdefaulttimeout(timeout)
+    # proxy_support = urllib.request.ProxyHandler(
+    #     {"http": "127.0.0.1:8080"})
+    # opener = urllib.request.build_opener(proxy_support)
+    # urllib.request.install_opener(opener)
+    response = urllib.request.urlopen(search_url).read().decode('utf-8')
+    json_response = json.loads(response)
+    
+
 
 if __name__ == "__main__":
     run_query('django')
