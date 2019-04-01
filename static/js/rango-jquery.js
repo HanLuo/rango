23958422bdead9c3a7546ff6b7049323b588a896
HanLuo <1903851304@qq.com>
2019-04-01 14:37:31 +0800

    update

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     $("#about-btn").click(function(event) {
         alert("You clicked the button using jQuery!");
     });
+
+    $("p").hover( function(){ 
+        $(this).css('color', 'red');
+    },
+    function(){
+        $(this).css('color', 'blue');
+    })
+
+    $("#about-btn").click( function(event) {
+        msgstr = $("#msg").html();
+        msgstr = msgstr + "000";
+        $("#msg").html(msgstr);
+    })
 });
