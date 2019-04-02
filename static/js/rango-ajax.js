e6b88046fe1540e64f1c1afc6aac4427de6b2ab7
HanLuo <1903851304@qq.com>
2019-04-02 18:42:05 +0800

    fix bug jQuery

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     $('#likes').click(function (){
         var catid;
         catid = $(this).attr("data-catid");
-        $.get('/rango/like_category', { category_id: catid}, function (data) {
+        $.get('/rango/like_category', { category_id: catid}, function(data) {
             $('#like_count').html(data);
             $('#likes').hide();
-        })
-    })
+        });
+    });
 });
